f4ecfbc5db7aded1f27c6d7cbc11f6dd65af5065
kconnell <connell.kate@ymail.com>
2019-02-28 23:54:31 -0500

    first upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('50 Ways to Leave Your Guava Burger');
+INSERT INTO burgers (burger_name) VALUES ('Peas and Thank You Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Is This Your Chard Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beet-er Late Than Never Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('So Many Fennel So Little Thyme Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Use It or Bleus It Burger');
